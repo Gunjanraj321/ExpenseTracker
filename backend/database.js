13842cd7bdea06cef8e1b7daaedcebfce628abca
gunjanraj321 <gunjanraj321@gmail.com>
2024-06-02 03:04:33 +0530

    pgsql updated

--- backend/database.js
+++ backend/database.js
@@ -1,28 +1,28 @@
 const Sequelize = require("sequelize");
 require('dotenv').config(); 
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-// const pg = require('pg');
+const pg = require('pg');
 // Connecting to the database using the Sequelize ORM (Object Relational Mapping)
-// const sequelize = new Sequelize(process.env.POSTGRES_URL, {
-//   dialectModule: pg,
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   },
-//   logging: false
-// });
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false
+});
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//     logging: false,
+//   }
+// );
 
 module.exports = sequelize;
